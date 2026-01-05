5f25157d34311594bdf1a2826a0279f08cce45e0
Adam Jenca <jenca.adam@gmail.com>
2026-01-05 19:23:42 +0100

    improved shutdown notice

--- app/static/js/poor.js
+++ app/static/js/poor.js
@@ -1,11 +1,12 @@
+const ENABLE_NOTICE=false;
 function showModalDialog(title, text){
     $("#modal-title").text(title);
     $("#modal-body").html(text);
     $("#modal-container").show();
 }
-if (!window.localStorage.getItem("shown_alert")){
+if (!window.localStorage.getItem("shown_alert")&&ENABLE_NOTICE){
 	window.localStorage.setItem("shown_alert", "yes :(");
-	showModalDialog("NOTICE", "gtedit.tech will be temporarily shutting down on the 12th of January.<br>The webhosting costs quite a bit of money that I don't get back, therefore I've decided to temporarily shut down this website, until I can find a less predatory hosting service that will suit my needs.<br>In the meantime, please refer to the Hosting locally section in the readme on GitHub.<br>Thanks for understanding.<br>&nbsp;&nbsp;&mdash;vcgd <3")
+	showModalDialog("NOTICE", "gtedit.tech will be temporarily shutting down on the 12th of January.<br>The webhosting costs quite a bit of money that I don't get back, therefore I've decided to temporarily shut down this website, until I can find a less predatory hosting service that will suit my needs.<br>In the meantime, please refer to the <a href=\"https://github.com/jenca-adam/gt_stats#hosting-locally\" target=\"_blank\">Hosting locally</a> section in the readme on GitHub.<br>Thanks for understanding.<br>&nbsp;&nbsp;&mdash;vcgd <3")
 }
 
 $("#modal-close").click(()=>{$("#modal-container").hide()});
